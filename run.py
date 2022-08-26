231a23db662119e5bcc57d8cf97c1b892e909b75
Bhfeirdi <ferdia.link@gmail.com>
2022-08-26 04:26:23 +0100

    create minefield class

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-# Importing packages
+class Minefield:
+    def __init__(self, dim_size, num_mines):
+        self.dim_size = dim_size
+        self.num_mines = num_mines
+
+
+def play(dim_size=8, num_mines=8):
+
+    pass
