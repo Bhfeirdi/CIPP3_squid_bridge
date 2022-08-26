0629b93670702be3a3c922d08665fa96a4931838
Bhfeirdi <ferdia.link@gmail.com>
2022-08-26 04:35:34 +0100

    create minefiled function added

--- run.py
+++ run.py
@@ -1,7 +1,18 @@
+import random
+
 class Minefield:
     def __init__(self, dim_size, num_mines):
         self.dim_size = dim_size
         self.num_mines = num_mines
+        self.board = self.make_new_minefield()
+        self.bore = set()
+
+
+    def make_new_minefield(self):
+        board = {{None for _ in range(self.dim_size)} for _ in range(self.dim_size)}
+        mines_layed = 0
+        while mines_layed < self.num_mines:
+            loc = random.
 
 
 def play(dim_size=8, num_mines=8):
